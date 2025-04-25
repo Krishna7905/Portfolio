b7fee9d6a85b61f73fbba273d441a3f8e24f613f
Krishna7905 <krishnaguptakg7905@gmail.com>
2025-04-25 08:37:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// skills section   
-const carouselTrack = document.querySelector('.carousel-track');
 
+
+// skills 
+
+const carouselTrack = document.querySelector('.carousel-track');
 carouselTrack.addEventListener('mouseenter', () => {
-  carouselTrack.style.animationPlayState = 'paused';
+    carouselTrack.style.animationPlayState = 'paused';
 });
 
 carouselTrack.addEventListener('mouseleave', () => {
-  carouselTrack.style.animationPlayState = 'running';
+    carouselTrack.style.animationPlayState = 'running';
 });
+
+
+// form 
+const neonButton = document.querySelector(".neon-button");
+
+neonButton.addEventListener("mouseenter", () => {
+    neonButton.style.boxShadow = "0 0 30px rgba(0, 255, 255, 1)";
+});
+
+neonButton.addEventListener("mouseleave", () => {
+    neonButton.style.boxShadow = "0 0 15px rgba(0, 255, 255, 0.8)";
+});
+
+
+// for timer
+
+function debounce(func, delay) {
+  let timer;
+  return function () {
+      clearTimeout(timer);
+      timer = setTimeout(func, delay);
+  };
+}
+
